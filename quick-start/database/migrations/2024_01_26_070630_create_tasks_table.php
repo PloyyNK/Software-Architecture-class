f8f0ec4aa27f3a0c156bed798683c2b6c6ec0617
PloyyNK <nartnatta_kri@yahoo.com>
2024-01-26 14:14:20 +0700

    add Task model

--- quick-start/database/migrations/2024_01_26_070630_create_tasks_table.php
+++ quick-start/database/migrations/2024_01_26_070630_create_tasks_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('user_id')->idex();
+            $table->string('name');
             $table->timestamps();
         });
     }
