c1bf573f0a3867b4b0b60124ae82c225a33760a6
PloyyNK <nartnatta_kri@yahoo.com>
2024-02-18 11:13:08 +0700

    modify task controller

--- quickstart/app/Http/Controllers/TaskController.php
+++ quickstart/app/Http/Controllers/TaskController.php
@@ -3,42 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
+use App\Repositories\TaskRepository; 
 use App\Models\Task;
-use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
-    /**
-     * The task repository instance.
-     *
-     * @var TaskRepository
-     */
     protected $tasks;
-    // protected $facebookPublisher;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @param  TaskRepository  $tasks
-     * @return void
-     */
     public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
-
         $this->tasks = $tasks;
     }
 
-    /**
-     * Display a list of all of the user's task.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
     public function index(Request $request)
     {
         return view('tasks.index', [
@@ -46,12 +23,6 @@ class TaskController extends Controller
         ]);
     }
 
-    /**
-     * Create a new task.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -65,13 +36,6 @@ class TaskController extends Controller
         return redirect('/tasks');
     }
 
-    /**
-     * Destroy the given task.
-     *
-     * @param  Request  $request
-     * @param  Task  $task
-     * @return Response
-     */
     public function destroy(Request $request, Task $task)
     {
         $this->authorize('destroy', $task);
